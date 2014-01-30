f132dda351a94deed5bcff58c28eaf0b50ad409c
wade12 <wadetaylor@engineer.com>
2014-01-30 16:30:57 +0000

    HashMap.

--- MVCLoginApp/src/com/wade12/service/LoginService.java
+++ MVCLoginApp/src/com/wade12/service/LoginService.java
@@ -1,8 +1,20 @@
 package com.wade12.service;
 
+import java.util.HashMap;
+
+// dummy business service
+// use a database in the real world!
 public class LoginService {
 	
-	// dummy method
+	HashMap<String, String> users = new HashMap<String, String>();
+	
+	public LoginService() {
+		users.put("Paddy", "Paddy Murphy");
+		users.put("Sean", "Sean Kelly");
+		users.put("Mary", "Mary Ryan");
+	} // end 0-argument constructor
+	
+	
 	public boolean authenticate(String userId, String password){
 		
 		if ( (password == null) || (password.trim() == "") ) {
@@ -10,7 +22,11 @@ public class LoginService {
 		} // end if
 		else
 			return true;
-		
 	} // end method authenticate
+	
+	
+	public String getUsername(String userId) {
+		return users.get(userId);
+	} // end method getUsername
 
 } // end Class LoginService
